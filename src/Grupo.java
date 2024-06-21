93a534a22ecc373ceb045fd22cd67b5065df6e7c
jojimenez78 <garciaalonso527@gmail.com>
2024-06-21 17:28:28 -0600

    Se agragaron los getter nd setter de la clase Grupo

---
+++ src/Grupo.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Extremetech SC
+ */
+public class Grupo {
+    private int idGrupo;
+    private String tipo,descripcion;
+
+    public Grupo(int idGrupo, String tipo, String descripcion) {
+        this.idGrupo = idGrupo;
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+    }
+
+    public int getIdGrupo() {
+        return idGrupo;
+    }
+
+    public void setIdGrupo(int idGrupo) {
+        this.idGrupo = idGrupo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+}
