5ac49b902c8d889cef51bfd5ddef88d95819d419
jojimenez78 <garciaalonso527@gmail.com>
2024-06-21 13:55:39 -0600

    se agrgaron los getter nd setter

--- src/redpoll/Usuario.java
+++ src/redpoll/Usuario.java
@@ -3,5 +3,56 @@ package redpoll;
 
 
 public class Usuario {
+    private int idUsuario,idRol;
+    private String nombre,correo,password;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(int idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Usuario(int idUsuario, int idRol, String nombre, String correo, String password) {
+        this.idUsuario = idUsuario;
+        this.idRol = idRol;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.password = password;
+    }
+    
     
 }
