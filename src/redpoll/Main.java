09f7ab5cdf86e5de15d78536b868f222b2ed366e
JEFTEDARIEL123 <jeftedariel3@gmail.com>
2024-06-21 12:36:41 -0600

    Método editarProduccion

--- src/redpoll/Main.java
+++ src/redpoll/Main.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package redpoll;
-
+import java.util.Date;
 /**
  *
  * @author jefte
@@ -14,7 +14,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Date fecha = new Date(20,2,2003);
+        Produccion uno = new Produccion(1,0,0,0,fecha);
+        
+        uno.editarProduccion(5, 799);
+        System.out.println(uno.getOrdeño_tarde());
     }
     
 }
