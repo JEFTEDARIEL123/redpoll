9eacb6f758c5a2b21f48ca11b8d7b991299446be
JEFTEDARIEL123 <jeftedariel3@gmail.com>
2024-06-21 12:40:24 -0600

    Produccion.java y métodos iniciales, método editar

---
+++ src/redpoll/Produccion.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package redpoll;
+
+import java.util.Date;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jefte
+ */
+
+public class Produccion {
+   private int id;
+   private double ordeño_mañana,ordeño_tarde, total;
+   private Date fecha;
+
+    public Produccion(int id, double ordeño_mañana, double ordeño_tarde, double total, Date fecha) {
+        this.id = id;
+        this.ordeño_mañana = ordeño_mañana;
+        this.ordeño_tarde = ordeño_tarde;
+        this.total = total;
+        this.fecha = fecha;
+    }
+    
+    public void editarProduccion(int seleccionEdit, double datoNuevo){
+        // El 1 equivale al dato de Ordeño_mañana
+        if(seleccionEdit == 1){
+            if(datoNuevo >= 0){
+                setOrdeño_mañana(datoNuevo);
+                JOptionPane.showMessageDialog(null, "Los datos se han actualizado exitosamente.", "Exito!", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Debe ingresar un dato mayor a 0", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        
+        if(seleccionEdit == 2){
+            if(datoNuevo >= 0){
+                setOrdeño_tarde(datoNuevo);
+                JOptionPane.showMessageDialog(null, "Los datos se han actualizado exitosamente.", "Exito!", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Debe ingresar un dato mayor a 0", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        
+        if(!(seleccionEdit == 1 || seleccionEdit == 2)){
+            JOptionPane.showMessageDialog(null, "El dato seleccionado para editar No es válido","Error", JOptionPane.ERROR_MESSAGE);
+        }
+        
+        
+    }
+    public int getId() {
+        return id;
+    }
+
+    public double getOrdeño_mañana() {
+        return ordeño_mañana;
+    }
+
+    public double getOrdeño_tarde() {
+        return ordeño_tarde;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setOrdeño_mañana(double ordeño_mañana) {
+        this.ordeño_mañana = ordeño_mañana;
+    }
+
+    public void setOrdeño_tarde(double ordeño_tarde) {
+        this.ordeño_tarde = ordeño_tarde;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+   
+   
+   
+   
+}
