075fd3c4dc748807cd1e62cbf31b743027c9f65c
JEFTEDARIEL123 <jeftedariel3@gmail.com>
2024-07-07 22:08:57 -0600

    reset main

--- src/redpoll/Main.java
+++ src/redpoll/Main.java
@@ -3,8 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package redpoll;
-import redpoll.login.InterfazLogin;
-import redpoll.Propietarios.GitPropietario;
 
 /**
  *
@@ -15,19 +13,8 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    
     public static void main(String[] args) {
-    java.awt.EventQueue.invokeLater(() -> {
-        new InterfazLogin().setVisible(true);
-    });
-}
-    
-//    public static void main(String[] args) {
-//        GitPropietario propietario = new GitPropietario();
-//        propietario.setVisible(true);
-//        propietario.setResizable(false);
-//        propietario.setLocationRelativeTo(null);
-//        
-//    }
+        // TODO code application logic here
+    }
     
 }
